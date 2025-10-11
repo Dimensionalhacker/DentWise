a92981b319f16af26db353f475f36e8b54de3a2c
BeastMask <manganyel48@gmail.com>
2025-10-11 16:32:19 +0200

    Landing page completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
-import { Button } from "@/components/ui/button";
 
-import { SignUpButton, SignedOut, SignedIn, SignOutButton } from "@clerk/nextjs";
-import Image from "next/image";
+import CTA from "@/components/ui/landing/CTA";
+import Header from "@/components/ui/landing/header";
+import Hero from "@/components/ui/landing/Hero";
+import HowItWorks from "@/components/ui/landing/HowItWorks";
+import PricingSection from "@/components/ui/landing/PricingSection";
+import WhatToAsk from "@/components/ui/landing/WhatToAsk";
+import { Footer } from "react-day-picker";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <SignedOut>
-        <SignUpButton mode="modal">Sign Up</SignUpButton>
-      </SignedOut>
+    <div className="min-h-screen bg-background">
+      <Header/>
+      <Hero/>
+      <HowItWorks/>
+      <WhatToAsk/>
+      <PricingSection/>
+      <CTA/>
+      <Footer/>
 
-      <SignedIn>
-          Logout
-      </SignedIn>
     </div>
   );
 }
